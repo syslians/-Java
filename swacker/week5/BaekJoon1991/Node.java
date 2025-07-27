42f35406782ff9f753c2165b498412f20930f7fa
Taewoo Lee <sai6321@naver.com>
2025-07-27 22:27:14 +0900

    5주차 백준 1991번 문제 풀이

---
+++ swacker/week5/BaekJoon1991/Node.java
@@ -0,0 +1,13 @@
+package week5.BaekJoon1991;
+
+class Node {
+    char value;
+    Node left;
+    Node right;
+
+    public Node(char value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
