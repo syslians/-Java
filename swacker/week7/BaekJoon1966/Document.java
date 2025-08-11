52c68c4575c703aa11ec5b59f6a71b0bb789907c
Taewoo Lee <sai6321@naver.com>
2025-08-11 21:50:04 +0900

    7주차

---
+++ swacker/week7/BaekJoon1966/Document.java
@@ -0,0 +1,10 @@
+package week7.BaekJoon1966;
+
+class Document {
+    int index;
+    int priority;
+    Document(int index, int priority) {
+        this.index = index;
+        this.priority = priority;
+    }
+}
